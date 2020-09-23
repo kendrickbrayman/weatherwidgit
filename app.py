cb44f4775e34aecac673edab3b5d792c9e2598c2
kendrickbrayman <kendrickbrayman@gmail.com>
2020-09-23 16:47:43 -0700

    test3

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route('/weather', methods=['GET','POST'])
 def webhook():
-    if 'lat' and 'long' in request.args:
+    if ('lat' and 'long' in request.args):
         lat = request.args['lat']
         long = request.args['long']
         weatherString = Weather(lat,long)
         return weatherString, 200
-    return 'OK', 200
+    return request.args, 200
 
 if __name__ == '__main__':
     app.run()
