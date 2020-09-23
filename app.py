0dac60f565d9449146aac1c0c1dea1dc4581c8c6
kendrickbrayman <kendrickbrayman@gmail.com>
2020-09-23 16:23:15 -0700

    added GET request and changed app route

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from WeatherWidgit import Weather
 app = Flask(__name__)
 
 
-@app.route('/WeatherWidgit', methods=['POST'])
+@app.route('/', methods=['GET,POST'])
 def webhook():
     if 'lat' and 'long' in request.args:
         lat = request.args['lat']
