61e542f4160666f46c20e2ed6151c198ff370784
kendrickbrayman <kendrickbrayman@gmail.com>
2020-09-23 18:08:02 -0700

    test3

--- WeatherWidgit.py
+++ WeatherWidgit.py
@@ -27,7 +27,7 @@ class Weather():
 
             outputText = 'The temperature tomorrow should be ' + str(nextDay["feels_like"]['day']) + u"\N{DEGREE SIGN}" + 'F'
 
-            return nextDay
+            return "outputText"
 
         except Exception as e:
             return str(e)
